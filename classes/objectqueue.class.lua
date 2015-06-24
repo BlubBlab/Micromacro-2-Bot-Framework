36f391b9112ad45edc850927baf34c6a07fb4e2c
BlubBlab <heroforfive@yahoo.de>
2015-06-25 01:56:59 +0200

    Update the objectqueue.class.lua to a real queue

--- classes/objectqueue.class.lua
+++ classes/objectqueue.class.lua
@@ -4,7 +4,8 @@ include("objectlists.settings.lua");
 CObjectQueue = class(
 	function (self)
 		self.Queue = {};
-		self.Pointer = ;
+		self.first = 0;
+		self.last = -1;
 	end
 );
 
@@ -16,65 +17,80 @@ function CObjectQueue:update()
 	for i = 0,size do
 		local addr = memoryReadUIntPtr(getProc(), addresses.staticTablePtr, i*4);
 		if( evalAddresse( addr )) then
-			self.Queue[i] = CObject(addr);
+			object = CObject(addr);
+			if(object ~= nil)then
+				self:add(object);
+			end
 		end
 	end
-	self.Pointer = #self.Queue;
+	
+	--self.Pointer = #self.Queue;
 end
 
 function CObjectQueue:peek()
-	if self.Pointer == 0 then
-		return nil;
-	end
+	local first = self.first:
 	
-	if(self.Queue[self.Pointer] ~= nil)then
+	if first > self.last then 
+		return nil;
+	end	
+	
+	if(self.Queue[first] ~= nil)then
 		--update object
-		self.Queue[self.Pointer]:update();
+		self.Queue[first]:update();
 		
-		if(self.Queue[self.Pointer]~=nil)then
-			return self.Queue[self.Pointer];
+		if(self.Queue[first] ~= nil)then
+			local value = self.Queue[first]
+			-- we peek only
+			--self.Queue[first] = nil        -- to allow garbage collection
+			--self.first = first + 1
+			return value;
 		else
-			self.Queue[self.Pointer] = nil;
-			self.Pointer = self.Pointer - self.Pointer;
+			self.Queue[first] = nil        -- to allow garbage collection
+			self.first = first + 1
 			return self:peek();	
 		end
 	else
-		self.Queue[self.Pointer] = nil;
-		self.Pointer = self.Pointer - self.Pointer;
+		self.Queue[first] = nil        -- to allow garbage collection
+		self.first = first + 1
 		return self:peek();	
 	end
 
 end
 
 function CObjectQueue:poll()
-	if self.Pointer == 0 then
-		return nil;
-	end
+	local first = self.first:
 	
-	if(self.Queue[self.Pointer] ~= nil)then
+	if first > self.last then 
+		return nil;
+	end	
+	
+	if(self.Queue[first] ~= nil)then
 		--update object
-		self.Queue[self.Pointer]:update();
+		self.Queue[first]:update();
 		
-		if(self.Queue[self.Pointer] ~= nil)then
-			-- we remove the header;
-			self.Queue[self.Pointer] = nil;
-			self.Pointer = self.Pointer - self.Pointer;
-			return self.Queue[self.Pointer];
+		if(self.Queue[first] ~= nil)then
+			local value = self.Queue[first]
+			self.Queue[first] = nil        -- to allow garbage collection
+			self.first = first + 1
+			return value;
 		else
-			self.Queue[self.Pointer] = nil;
-			self.Pointer = self.Pointer - self.Pointer;
-			return self:peek();	
+			self.Queue[first] = nil        -- to allow garbage collection
+			self.first = first + 1
+			return self:poll();	
 		end
 	else
-		self.Queue[self.Pointer] = nil;
-		self.Pointer = self.Pointer - self.Pointer;
-		return self:peek();	
+		self.Queue[first] = nil        -- to allow garbage collection
+		self.first = first + 1
+		return self:poll();	
 	end
 end
-
+-
 function CObjectQueue:add(object)
-	self.Pointer = self.Pointer + self.Pointer;
-	self.Queue[self.Pointer] = object;
+	
+	--pushright
+	local last = self.last + 1
+	self.last = last;
+	self.Queue[last] = object;
 end
 
 function CObjectQueue:size()
